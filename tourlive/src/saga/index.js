b6aabca7f7c3d7c4a36642ec805c4a17b811a711
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-09-21 00:18:52 +0900

    RESET ALL

--- tourlive/src/saga/index.js
+++ tourlive/src/saga/index.js
@@ -5,7 +5,6 @@ import boardSaga from "./board";
 
 axios.defaults.baseURL =
   "http://tourlive-external-1isp315cijj1v-591526764.ap-northeast-2.elb.amazonaws.com:8888/";
-axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
   yield all([fork(boardSaga)]);
--- tourlive/src/store/configureStore.js
+++ tourlive/src/store/configureStore.js
@@ -1,6 +1,5 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 // import reducer from "../reducers/goods";
 
@@ -10,11 +9,7 @@ import rootSaga from "../saga/index";
 
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
-  // const middlewares = [sagaMiddleware];
-  // const enhancer =
-  //   process.env.NODE_ENV === "production"
-  //     ? compose(applyMiddleware(...middlewares))
-  //     : composeWithDevTools(applyMiddleware(...middlewares));
+
   const store = createStore(reducer, applyMiddleware(sagaMiddleware));
   store.sagaTask = sagaMiddleware.run(rootSaga);
 
