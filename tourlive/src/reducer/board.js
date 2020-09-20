875fdf2dbd2e8a707a8c507cf75e364804a56aa8
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-09-21 00:13:37 +0900

    UPDATE reducer/board

--- tourlive/src/reducer/board.js
+++ tourlive/src/reducer/board.js
@@ -1,19 +1,37 @@
 export const initialState = {
   board: [],
+  searchList: [],
 
   loadBoardLoading: false,
   loadBoardDone: false,
   loadBoardError: null,
+
+  loadSearchListLoading: true,
+  loadSearchListDone: false,
+  loadSearchListError: null,
 };
 
 export const LOAD_BOARD_REQUEST = "LOAD_BOARD_REQUEST";
 export const LOAD_BOARD_SUCCESS = "LOAD_BOARD_SUCCESS";
 export const LOAD_BOARD_FAILURE = "LOAD_BOARD_FAILURE";
 
-export const loadBoard = () => {
-  console.log("In REDUCER, loadBoard, executes ");
+export const LOAD_SEARCHLIST_REQUEST = "LOAD_SEARCHLIST_REQUEST"; // search list
+export const LOAD_SEARCHLIST_SUCCESS = "LOAD_SEARCHLIST_SUCCESS";
+export const LOAD_SEARCHLIST_FAILURE = "LOAD_SEARCHLIST_FAILURE";
+
+export const loadBoard = (data) => {
+  console.log("In REDUCER, loadBoard, data ", data);
   return {
     type: LOAD_BOARD_REQUEST,
+    data,
+  };
+};
+
+export const loadSearchList = (keyword) => {
+  console.log("In REDUCER,loadSerchList, data : ", keyword);
+  return {
+    type: LOAD_SEARCHLIST_REQUEST,
+    keyword,
   };
 };
 
@@ -33,8 +51,7 @@ const reducer = (state = initialState, action) => {
         ...state,
         loadBoardLoading: false,
         loadBoardDone: true,
-        loadBoardError: null,
-        review: [action.data, ...state.review],
+        board: action.data,
       };
     case LOAD_BOARD_FAILURE:
       console.log("In REDUCER, LOAD_BOARD_FAILURE action : ", action);
@@ -44,6 +61,31 @@ const reducer = (state = initialState, action) => {
         loadBoardError: action.error,
         review: state.board.filter((el) => el.id !== "not found"),
       };
+
+    case LOAD_SEARCHLIST_REQUEST:
+      console.log("In REDUX, LOAD_SEARCHLIST_REQUEST, action : ", action);
+      return {
+        ...state,
+        loadSearchListLoading: true, // search list
+        loadSearchListDone: false,
+        loadSearchListError: null,
+      };
+    case LOAD_SEARCHLIST_SUCCESS:
+      console.log("In REDUX, LOAD_SEARCHLIST_SUCCESS, action : ", action);
+      return {
+        ...state,
+        loadSearchListLoading: false, // goods list
+        loadSearchListDone: true,
+        searchList: action.data,
+      };
+    case LOAD_SEARCHLIST_FAILURE:
+      console.log("In REDUX, LOAD_SEARCHLIST_FAILURE, action : ", action);
+      return {
+        ...state,
+        loadSearchListLoading: false,
+        loadSearchListError: action.error,
+      };
+
     default:
       return state;
   }
