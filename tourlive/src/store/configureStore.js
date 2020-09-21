1f37d8ded4813be4ebb9b60d6e319ea8e6ca5d1c
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-09-21 15:28:23 +0900

    SIMPLY store

--- tourlive/src/store/configureStore.js
+++ tourlive/src/store/configureStore.js
@@ -1,10 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
-
-// import reducer from "../reducers/goods";
-
 import reducer from "../reducer/board";
-
 import rootSaga from "../saga/index";
 
 const configureStore = () => {
